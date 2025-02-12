6bcd5c14e2c2a44c52d109f6086f4ab947e0a0d3
Oscar2343 <114583270+Oscar2343@users.noreply.github.com>
2025-02-13 00:02:39 +0100

    Update MainTest.java

--- src/test/java/group6/MainTest.java
+++ src/test/java/group6/MainTest.java
@@ -1,1 +1,11 @@
+package group6;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    void trivialTest() {
+        assertTrue(true);
+    }
+}
